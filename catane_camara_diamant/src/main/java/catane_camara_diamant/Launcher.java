f70ceb16260347d3a0305e20f97124fa3ece3b89
Alexandre <diamant.alexandre@gmail.com>
2021-12-10 16:15:51 +0100

    classes necessaires

---
+++ catane_camara_diamant/src/main/java/catane_camara_diamant/Launcher.java
@@ -0,0 +1,9 @@
+package catane_camara_diamant;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ catane_camara_diamant/src/main/java/catane_camara_diamant/Player.java
@@ -0,0 +1,5 @@
+package catane_camara_diamant;
+
+public class Player {
+    
+}
